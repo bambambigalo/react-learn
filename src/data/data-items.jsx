79432213e8d2a0672cb649ba5827120f921da197
AndrLavr Bambambigalo <65401496+bambambigalo@users.noreply.github.com>
2022-08-11 14:51:58 +0300

    clear

--- src/data/data-items.jsx
+++
@@ -1,8 +0,0 @@
-export const items = [
-    {id:1, title:'Яблоко', subtitle:'Очень вкусное яблоко', price:'2$', key:1},
-    {id:1, title:'Апельсин', subtitle:'Очень вкусный апельсин', price:'3$', key:2},
-    {id:1, title:'Банан', subtitle:'Очень вкусный банан', price:'1$', key:3},
-    {id:1, title:'Ананас', subtitle:'Очень вкусный ананас', price:'5$', key:4},
-    {id:1, title:'Виноград', subtitle:'Очень вкусный виноград', price:'3$', key:5},
-    {id:1, title:'Манго', subtitle:'Очень вкусное манго', price:'4$', key:6},
-]
